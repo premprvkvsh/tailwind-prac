f014437a6edffd369c7fd58c9b52556677d24d1b
Prem <pprakash312003@gmail.com>
2024-08-09 18:03:17 +0530

    tailwind flex, grid

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='grid grid-cols-3'>
+        <div className='bg-red-500'>Item 1</div>
+        <div className='bg-yellow-500'>Item 2</div>
+        <div className='bg-green-500'>Item 3</div>
+      
+      </div> 
+    </>
+  )
+}
+
+
+export default App
+
+

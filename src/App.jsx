f24bb687788934270889eee698fddddad1688d1e
Prem <pprakash312003@gmail.com>
2024-08-09 19:22:16 +0530

    mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
   return (
     <>
-      <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
         <div className='bg-red-500'>Item 1</div>
         <div className='bg-yellow-500'>Item 2</div>
         <div className='bg-green-500'>Item 3</div>
-      
-      </div> 
+        <div className='bg-blue-500'>Item 4</div>  
+      </div>
     </>
   )
 }
-
 
 export default App
 
